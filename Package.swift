bece87161b56ce209b6d3fefd566e06e72f53b39
Will McGinty <mcgintw@gmail.com>
2018-05-11 17:59:45 -0500

    Realign project name with repo name

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Vapor-Auth",
+    name: "VaporAuth",
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
